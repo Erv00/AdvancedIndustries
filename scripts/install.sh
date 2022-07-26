388449d43e143bd8d0a257e51fb0d224688b185a
Varga Áron <vargaaron2003@gmail.com>
2022-07-26 21:52:46 +0200

    fixed scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-FACTORIO_FOLDER="$HOME/.factorio"
+
+FACTORIO_FOLDER=("$HOME/.factorio" "$HOME/.var/app/com.valvesoftware.Steam/.factorio") 
+for i in ${FACTORIO_FOLDER[@]}; do
+	if [ -d "$i" ]; then
+  		FACTORIO_FOLDER=$i
+		break
+	fi
+done
 
 echo "linking" "$SCRIPT_DIR/../advanced-industries/" "$FACTORIO_FOLDER/mods/"
-
 ln -s $SCRIPT_DIR/../advanced-industries/ $FACTORIO_FOLDER/mods/
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-FACTORIO_FOLDER="$HOME/.factorio"
+FACTORIO_FOLDER=("$HOME/.factorio" "$HOME/.var/app/com.valvesoftware.Steam/.factorio")
+for i in ${FACTORIO_FOLDER[@]}; do
+	if [ -d "$i" ]; then
+  		FACTORIO_FOLDER=$i
+		break
+	fi
+done
+
 unlink $FACTORIO_FOLDER/mods/advanced-industries
